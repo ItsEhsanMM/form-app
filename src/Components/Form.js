bc943058b26dd4f3f6ad01d5c2c60c9d5786698d
Ehsan Mohammadi <itsehsanmm@gmail.com>
2023-05-15 18:01:14 +0330

    inputs clickHandler has finished

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Form = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    pass: "",
+    confirmpass: "",
+    isAgreed: false,
+  });
+  const { name, email, pass, confirmpass, isAgreed } = data;
+
+  const changeHandler = (event) => {
+    if (event.target.name === "isAgreed") {
+      setData({ ...data, isAgreed: !isAgreed });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+  };
+
   return (
     <div>
       <form>
@@ -8,25 +25,52 @@ const Form = () => {
 
         <div>
           <label>UserName :</label>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={changeHandler}
+          />
         </div>
 
         <div>
           <label>Email :</label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={changeHandler}
+          />
         </div>
+
         <div>
           <label>Password :</label>
-          <input type="password" name="pass" />
+          <input
+            type="password"
+            name="pass"
+            value={pass}
+            onChange={changeHandler}
+          />
         </div>
+
         <div>
           <label>Confirm Password :</label>
-          <input type="text" name="confirmpass" />
+          <input
+            type="text"
+            name="confirmpass"
+            value={confirmpass}
+            onChange={changeHandler}
+          />
         </div>
 
         <div>
           <label>I accept agreement</label>
-          <input type="checkbox" name="agreement" />
+          <input
+            type="checkbox"
+            name="isAgreed"
+            value={isAgreed}
+            onChange={changeHandler}
+          />
         </div>
 
         <a href="#">Login</a>
